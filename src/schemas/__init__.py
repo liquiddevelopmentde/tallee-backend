f4083742618a719ed95a4435b92aa7884b9c94b6
gelbeinhalb <exzitonyw@gmail.com>
2026-06-25 11:08:44 +0200

    delete old layout

--- src/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Pydantic request/response schemas."""
-
-from .share import ErrorResponse, SharePayload, ShareCreateResponse, ShareResponse
-
-__all__ = ["ErrorResponse", "SharePayload", "ShareCreateResponse", "ShareResponse"]
