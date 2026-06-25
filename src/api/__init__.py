e2eb8a7053d39fef8392ddbf2e2b79eea58fe7fe
gelbeinhalb <exzitonyw@gmail.com>
2026-06-25 11:09:21 +0200

    add Token

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,9 +1,11 @@
 from . import endpoints
 from . import handlers
 from .limiter import limiter
+from .token import Token
 
 __all__ = [
     "endpoints",
     "handlers",
     "limiter",
+    "Token",
 ]
