d3b90c0f28ff970171850e0898dcd05e51520d3d
gelbeinhalb <exzitonyw@gmail.com>
2026-06-25 10:58:31 +0200

    fix gunicorn not finding api

--- src/main.py
+++ src/main.py
@@ -9,30 +9,26 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from services import MemoryStore
 
-
+"""
+This is scheduled for deletion :) Im planning on using Redis instead
+"""
 @asynccontextmanager
-async def lifespan(api: FastAPI):
+async def lifespan(_api: FastAPI):
     store: MemoryStore = MemoryStore()
     await store.startup()
-    api.state.store = store
+    _api.state.store = store
     try:
         yield
     finally:
         await store.shutdown()
 
+api: FastAPI = FastAPI(
+    title=config.API_TITLE,
+    version=config.API_VERSION,
+    summary=config.API_SUMMARY,
+    lifespan=lifespan
+)
 
-def main() -> None:
-    api: FastAPI = FastAPI(
-        title=config.API_TITLE,
-        version=config.API_VERSION,
-        summary=config.API_SUMMARY,
-        lifespan=lifespan
-    )
-    api.state.limiter = limiter
-
-    handlers.register_exception_handlers(api)
-    handlers.register_middleware_handlers(api)
-
-
-if __name__ == "__main__":
-    main()
+api.state.limiter = limiter
+handlers.register_exception_handlers(api)
+handlers.register_middleware_handlers(api)
