78636f0fc80b5bea9268570071c63f2ad0213139
gelbeinhalb <exzitonyw@gmail.com>
2026-06-25 19:00:15 +0200

    update data store

--- src/data/store.py
+++ src/data/store.py
@@ -1,5 +1,5 @@
 """
-A single data entry
+All Data Entries
 """
 from api import Token
 from redis import asyncio as aioredis
@@ -7,12 +7,12 @@ from typing import Optional
 from typing import Union
 
 
-class Entry:
+class Store:
 
     def __init__(self, client: aioredis.Redis):
         self.client: aioredis.Redis = client
 
-    async def set(self, token: Token, payload: str, ttl: int) -> None:
+    async def add(self, token: Token, payload: str, ttl: int) -> None:
         await self.client.set(token.value, payload, ex=ttl)
 
     async def get(self, token: Token) -> Optional[Union[str, bytes]]:
